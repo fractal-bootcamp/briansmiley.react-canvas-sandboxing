4444ca6e0a2a449c80949b47aa070a4779767e80
Brian Smiley <briantsmiley42@gmail.com>
2024-06-13 17:29:52 -0400

    implements basic hook into canvas/sketch from react

--- src/assets/p5Sketch.ts
+++ src/assets/p5Sketch.ts
@@ -3,10 +3,10 @@ export const sketch = (s: p5) => {
   s.setup = () => {
     s.createCanvas(500, 500);
     s.background(200);
+    s.fill("red");
   };
   s.draw = () => {
     //   s.background(255);
-    s.fill("red");
     if (s.mouseIsPressed) s.circle(s.mouseX, s.mouseY, 50);
   };
   s.mousePressed = () => {
--- src/components/P5Canvas.tsx
+++ src/components/P5Canvas.tsx
@@ -1,15 +1,21 @@
 import p5 from "p5";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { sketch } from "../assets/p5Sketch";
 
 const P5Canvas = () => {
+  const [p5Instance, setP5Instance] = useState<p5 | null>(null);
   const canvasRef = useRef<HTMLDivElement>(null);
-
+  const testColorSwitch = (p5Instance: p5) => p5Instance.fill(0);
   useEffect(() => {
-    const p5Instance = new p5(sketch, canvasRef.current!);
-    return p5Instance.remove;
+    const myP5: p5 = new p5(sketch, canvasRef.current!);
+    setP5Instance(myP5);
+    return myP5.remove;
   }, []);
-  return <div ref={canvasRef}>Canvas Goes here</div>;
+  return (
+    <div ref={canvasRef} onClick={() => testColorSwitch(p5Instance!)}>
+      Canvas Goes here
+    </div>
+  );
 };
 
 export default P5Canvas;
